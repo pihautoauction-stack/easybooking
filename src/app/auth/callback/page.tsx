b3db3b0b2994196ea201ce765ae770932fad6069
Anton <pih.autoauction@gmail.com>
2026-02-13 21:28:50 +0100

    2128

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -4,7 +4,8 @@ import { useEffect } from "react";
 export default function AuthCallback() {
   useEffect(() => {
     // После подтверждения почты, просто перекидываем в бота
-    window.location.href = "https://t.me/ТВОЙ_БОТ_BOT/app"; 
+    window.location.href = "https://easybooking-omega.vercel.app/auth/callback"
+"; 
   }, []);
 
   return <div className="min-h-screen bg-black text-white flex items-center justify-center">Вход выполнен! Возвращаемся в Telegram...</div>;
